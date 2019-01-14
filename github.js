3ccda41890a5cf1f22ae77b1d31d1fbc039182c1
Abdulahi Roble <abdulahi_2610@hotmail.com>
2019-01-14 13:11:57 +0100

    github finder

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = 'da80dc612c9947fa5abc';
+    this.client_secret = '398b93678808baa247b0fccec2fb7eaf4dfcfeb8';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id.id}client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
